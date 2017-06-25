2c97400cb2fc09af72cb09cd3f804e124f80f083
joshita <joshita@minedbytes.com>
2017-06-25 22:43:21 +0530

    initial commit

---
+++ src/app/index.module.js
@@ -0,0 +1,8 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('selfieapp', ['ngSanitize','ngMessages','ngAria','ui.router', 'ui.bootstrap', 'toastr','formio',
+    'ngFormioHelper']);
+
+})();
